c90ee91484765f218c61efec79a6a668a9ee8123
Your Name <you@example.com>
2026-06-18 17:49:10 +0300

    fix: move yandex metrika to site layout only, add favicon, remove from root layout

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 
 import { getPayload } from 'payload'
 
@@ -25,6 +26,9 @@ export const metadata: Metadata = {
   robots: 'index, follow',
   alternates: {
     canonical: 'https://wdynasty.ru',
+  },
+  icons: {
+    icon: '/favicon.ico',
   },
   openGraph: {
     type: 'website',
@@ -92,6 +96,20 @@ export default async function RootLayout({
           <Navbar items={navResult.docs as any} />
           <main>{children}</main>
         </Providers>
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`(function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109964523', 'ym');
+          ym(109964523, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});`}
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/109964523" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
       </body>
     </html>
 
